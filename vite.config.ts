4aa84cfda4ee17f088f27f3ce9c1d6f3d5c719ea
Nouchitic <nouchipri@outlook.com>
2026-07-01 21:14:57 +0800

    补充userscript字段

--- vite.config.ts
+++ vite.config.ts
@@ -11,10 +11,12 @@ export default defineConfig({
             userscript: {
                 name: "B站直播表情助手",
                 icon: "http://bilibili.com/favicon.ico",
-                namespace: "npm/vite-plugin-monkey",
+                namespace: "https://github.com/Nouchi-Kousu/bilibili_live_emoticon_helper",
                 match: ["https://live.bilibili.com/*"],
                 grant: "GM_cookie",
                 connect: ["api.live.bilibili.com"],
+                description: "B站直播间表情辅助脚本",
+                license: "MIT",
             },
         }),
     ],
